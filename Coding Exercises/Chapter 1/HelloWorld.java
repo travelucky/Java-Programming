668d0962b3df3091e3b2d1c423ac1ab22af62c7f
Fighting <19401469@qq.com>
2024-09-17 15:53:42 +0800

    Delete Coding Exercises/Chapter 1/HelloWorld.java

--- Coding Exercises/Chapter 1/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.my.study;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Programming is fun!");
-        System.out.println("Fundamentals first");
-        System.out.println("Problem driven");
-    }
-}
