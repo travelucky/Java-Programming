6e01931ad07f4f3e4bb6743cb28323a62626d1ba
Fighting <19401469@qq.com>
2024-09-17 16:20:41 +0800

    Delete Coding Exercises/Chapter 1/Area_and_perimeter_of_a_rectangle.java

--- Coding Exercises/Chapter 1/Area_and_perimeter_of_a_rectangle.java
+++
@@ -1,9 +0,0 @@
-package com.first.learn.chapter1;
-
-public class Area_and_perimeter_of_a_rectangle {
-    public static void main(String[] args) {
-        double width = 4.5,height = 3.5;
-        double area = width * height;
-        System.out.println("The area of the rectangle is " + area);
-    }
-}
