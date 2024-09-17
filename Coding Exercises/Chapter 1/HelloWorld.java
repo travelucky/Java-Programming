69057d396602f53b62ce9b2ab1d050db48fc7b30
Fighting <19401469@qq.com>
2024-09-17 14:57:30 +0800

    Add files via upload

---
+++ Coding Exercises/Chapter 1/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.my.study;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Programming is fun!");
+        System.out.println("Fundamentals first");
+        System.out.println("Problem driven");
+    }
+}
