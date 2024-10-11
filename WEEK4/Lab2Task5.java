62b430b098f7938714a9660157e28f7df4bfb747
Fighting <19401469@qq.com>
2024-10-11 21:31:42 +0800

    Update Lab2Task5.java

--- WEEK4/Lab2Task5.java
+++ WEEK4/Lab2Task5.java
@@ -1,4 +1,11 @@
 package com.first.learn.WEEK4;
 
 public class Lab2Task5 {
+   Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the subtotal and the gratuity rate:");
+        double subtotal = sc.nextDouble();
+        double gratuity_rate = sc.nextDouble() / 100;
+        double gratuity = subtotal * gratuity_rate;
+        double total = subtotal + gratuity;
+        System.out.println("The gratuity is " + gratuity + ", and the total is $" + total);
 }
