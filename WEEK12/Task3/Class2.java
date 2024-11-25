bb44d97d10812190187c68010ab3484f486c5ab5
Fighting <19401469@qq.com>
2024-11-25 14:18:04 +0800

    Add files via upload

---
+++ WEEK12/Task3/Class2.java
@@ -0,0 +1,10 @@
+package Task3;
+
+public class Class2 {
+    public static void main(String[] args) {
+        Class1 c1 = new Class1();
+        System.out.println(c1.x);
+        System.out.println(c1.y);
+    }
+}
+
